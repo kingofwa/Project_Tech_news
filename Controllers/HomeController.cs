c4cb1da0c7fdafe1fb2d4b9e75c8998da2366f7e
le nguyenvan <lenguyenvan09@gmail.com>
2020-12-19 23:11:36 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Project_Tech_News.DAO;
+using Project_Tech_News.Models.FW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_Tech_News.Controllers
+{
+    public class HomeController : Controller
+    {
+        News_TechEntities db = new News_TechEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        [ChildActionOnly]//chi duoc nhung , khong duoc goi.
+        public ActionResult Footer()
+        {
+            return PartialView();
+        }
+        [ChildActionOnly]
+        public ActionResult Header()
+        {
+            var list_danhmucmenu = db.Danh_muc.Where(x => x.Status == 1).ToList();
+            return PartialView(list_danhmucmenu);
+        }
+
+        [ChildActionOnly]
+        public ActionResult Header_mobile()
+        {
+            var list_danhmucmenu = db.Danh_muc.Where(x => x.Status == 1).ToList();
+            return PartialView(list_danhmucmenu);
+        }
+    }
+}
---
+++ DAO/Menu.cs
@@ -0,0 +1,18 @@
+﻿using Project_Tech_News.Models.FW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_Tech_News.DAO
+{
+    public class MenuDAO
+    {
+        News_TechEntities db = null;
+        public MenuDAO() 
+        {
+            db = new News_TechEntities();
+        }
+        
+    }
+}
